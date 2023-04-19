a0068b7f6c573cc222aa73b8e87e394c606318d4
ingsantiagoq <82769625+ingsantiagoq@users.noreply.github.com>
2023-04-19 07:34:31 -0500

    Clear Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container...
 builder.Services.AddControllersWithViews();
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
@@ -26,24 +25,9 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 
-
-//app.Use(async (context, next) =>
-//{
-//    // Agregar cabecera Content-Security-Policy
-//    context.Response.Headers.Add("Content-Security-Policy",
-//        "default-src 'self' https://localhost:7092");
-
-//    // Pasar la solicitud al siguiente middleware
-//    await next.Invoke();
-//});
-
-
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -60,8 +44,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
 
 
 app.Run();
